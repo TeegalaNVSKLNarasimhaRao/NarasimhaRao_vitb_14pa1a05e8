10947b096ae777141295861f68f1b83dd22c0def
TeegalaNVSKLNarasimhaRao <31816848+TeegalaNVSKLNarasimhaRao@users.noreply.github.com>
2017-09-12 01:35:28 -0700

    mc

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -162,7 +162,15 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public void bubbleSort() {
-		// Add your implementation here
+		for(int i=0;i<students.length;i++) {
+			for(int j=0;j<students.length-i-1;j++) {
+				if(students[j]>students[j+1]) {
+					Student temp=students[j];
+					students[j]=students[j+1];
+					students[j+1]=temp;
+				}
+			}
+		}
 	}
 
 	@Override
